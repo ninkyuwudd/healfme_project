4e87cb35dffa60395236e3bd0a3fa88a7bc8064c
NinKyuWudd <116327302+ninkyuwudd@users.noreply.github.com>
2023-05-23 20:31:54 +0700

    tambah api berita

--- lib/widget/homepage/icon_grid.dart
+++ lib/widget/homepage/icon_grid.dart
@@ -4,15 +4,17 @@ import 'package:flutter/material.dart';
 class IconGrid extends StatelessWidget {
   final String img;
   final String poli;
-  IconGrid({required this.img,required this.poli});
+  final Function() fungsi;
+  IconGrid({required this.img,required this.poli, required this.fungsi});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: fungsi,
       child: Container(
         child: Column(
           children: [
-            Image(image: AssetImage("images/$img.png"),width: 90,),
+            Image(image: AssetImage("images/$img.png"),width: 80,),
             SizedBox(height: 5,),
             Expanded(child: Text("$poli",style: TextStyle(fontSize: 12,fontWeight: FontWeight.normal),))
           ],
